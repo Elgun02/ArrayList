a7cb0cf82438de7bafa06586c96b29903ff3cb4e
Elgun <eabdulaev12@mail.ru>
2023-09-16 02:05:09 +0300

    commit

---
+++ src/ArrayLists.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ArrayLists {
+    private final Random random = new Random();
+    private final ArrayList<Integer> arr = new ArrayList<>();
+    private final ArrayList<Integer> list = new ArrayList<>();
+    public void AddRandomNumbers() {
+        for (int i = 0; i < 100; i++) {
+            arr.add(random.nextInt(100));
+        }
+    }
+
+    public void InOrderMax() {
+        for (int i = 1; i < arr.size(); i++) {
+            int currentElement = arr.get(i);
+            int previousElement = arr.get(i - 1);
+
+            if (currentElement > previousElement) {
+                System.out.print(currentElement + ", ");
+            }
+        }
+    }
+
+    public void AddNonPositiveNumbers() {
+        for (int i = 0; i < 100; i++) {
+            list.add(random.nextInt(-100,100));
+        }
+    }
+
+    public void InOrderMin() {
+        int minNumber = 101;
+
+        for(int numbers : list){
+            if (numbers > 0 && numbers < minNumber) {
+                minNumber = numbers;
+            }
+        }
+
+        System.out.println(minNumber);
+    }
+
+    public ArrayList<Integer> getArr() {
+        return arr;
+    }
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+
+}
